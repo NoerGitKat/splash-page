9bf0549be9d1e55e6ce5bbc4653ac362e0d69368
Noer Paanakker <noer@lightworksweb.nl>
2020-10-25 21:59:53 +0100

    added bg change toggle button

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log("hello!");
+const { body } = document;
+function changeBackground(bgNumber) {
+    // If body has a class, set it to variable
+    let prevBg = "background-1";
+    if (body.className) {
+        prevBg = body.className;
+    }
+    body.className = "";
+    switch (bgNumber) {
+        case "1":
+            return prevBg === "background-1"
+                ? false
+                : body.classList.add("background-1");
+        case "2":
+            return prevBg === "background-2"
+                ? false
+                : body.classList.add("background-2");
+        case "3":
+            return prevBg === "background-3"
+                ? false
+                : body.classList.add("background-3");
+        default:
+            return prevBg === "background-1"
+                ? false
+                : body.classList.add("background-1");
+    }
+}
+localStorage;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-"use strict";
+const { body } = document;
 
-console.log("hello!");
+function changeBackground(bgNumber: string): boolean | void {
+  // If body has a class, set it to variable
+  let prevBg: string = "background-1";
+  if (body.className) {
+    prevBg = body.className;
+  }
+
+  body.className = "";
+  switch (bgNumber) {
+    case "1":
+      return prevBg === "background-1"
+        ? false
+        : body.classList.add("background-1");
+    case "2":
+      return prevBg === "background-2"
+        ? false
+        : body.classList.add("background-2");
+    case "3":
+      return prevBg === "background-3"
+        ? false
+        : body.classList.add("background-3");
+    default:
+      return prevBg === "background-1"
+        ? false
+        : body.classList.add("background-1");
+  }
+}
+
+localStorage;
